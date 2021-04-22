619f9848d36d069060125f587bb5f3f21a8415f4
Protocol Corporation <protocollive@outlook.com>
2021-04-22 20:18:16 -0300

    Auto event type selection

--- src/factorys.php
+++ src/factorys.php
@@ -1,59 +1,15 @@
 <?php
-// 2021.04.17.00
+// 2021.04.22.00
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/TelegramBot
+
+abstract class TelegramBot_FactoryEvent{
+  public int $Type = 0;
+}
 
 class TelegramBot_FactoryServer{
   public int $Id = 0;
   public object $Event;
-}
-
-class TelegramBot_FactoryEventText{
-  public int $Type = 0;
-  public int $Id = 0;
-  public object $User;
-  public object $Chat;
-  public string $Msg;
-  public ?int $Reply = null;
-}
-
-class TelegramBot_FactoryEventVoice{
-  public int $Type = 0;
-  public int $Id = 0;
-  public string $File;
-}
-
-class TelegramBot_FactoryEventImage{
-  public int $Type = 0;
-  public int $Id = 0;
-  public object $User;
-  public string $File;
-  public string $Minuature;
-}
-
-class TelegramBot_FactoryEventDocument{
-  public int $Type = 0;
-  public int $Id = 0;
-  public object $User;
-  public string $File;
-  public string $Name;
-}
-
-class TelegramBot_FactoryEventGroupMe{
-  public int $Type = 0;
-  public int $Action = 0;
-}
-
-class TelegramBot_FactoryEventGroupUpdate{
-  public int $Type = 0;
-  public int $Action = 0;
-}
-
-class TelegramBot_FactoryEventCallback{
-  public int $Type = 0;
-  public int $Id = 0;
-  public object $User;
-  public string $Data;
 }
 
 class TelegramBot_FactoryUser{
@@ -71,4 +27,87 @@ class TelegramBot_FactoryChat{
   public string $Name;
   public ?string $NameLast = null;
   public ?string $NameUser = null;
+}
+
+class TelegramBot_FactoryEventText extends TelegramBot_FactoryEvent{
+  public int $Id = 0;
+  public object $User;
+  public object $Chat;
+  public string $Msg;
+  public ?int $Reply = null;
+  public function __construct(){
+    $this->Type = TelegramBot_Basics::Event_Text;
+    $this->User = new TelegramBot_FactoryUser;
+    $this->Chat = new TelegramBot_FactoryChat;
+  }
+}
+
+class TelegramBot_FactoryEventCommand extends TelegramBot_FactoryEvent{
+  public int $Id = 0;
+  public object $User;
+  public object $Chat;
+  public string $Command;
+  public ?string $Parameters = null;
+  public string $Msg;
+  public function __construct(){
+    $this->Type = TelegramBot_Basics::Event_Command;
+    $this->User = new TelegramBot_FactoryUser;
+    $this->Chat = new TelegramBot_FactoryChat;
+  }
+}
+
+class TelegramBot_FactoryEventVoice extends TelegramBot_FactoryEvent{
+  public int $Id = 0;
+  public string $File;
+  public function __construct(){
+    $this->Type = TelegramBot_Basics::Event_Voice;
+  }
+}
+
+class TelegramBot_FactoryEventImage extends TelegramBot_FactoryEvent{
+  public int $Id = 0;
+  public object $User;
+  public string $File;
+  public string $Minuature;
+  public function __construct(){
+    $this->Type = TelegramBot_Basics::Event_Image;
+    $this->User = new TelegramBot_FactoryUser;
+  }
+}
+
+class TelegramBot_FactoryEventDocument extends TelegramBot_FactoryEvent{
+  public int $Id = 0;
+  public object $User;
+  public string $File;
+  public string $Name;
+  public function __construct(){
+    $this->Type = TelegramBot_Basics::Event_Document;
+    $this->User = new TelegramBot_FactoryUser;
+  }
+}
+
+class TelegramBot_FactoryEventCallback extends TelegramBot_FactoryEvent{
+  public int $Id = 0;
+  public object $User;
+  public object $Chat;
+  public string $Data;
+  public function __construct(){
+    $this->Type = TelegramBot_Basics::Event_Callback;
+    $this->User = new TelegramBot_FactoryUser;
+    $this->Chat = new TelegramBot_FactoryChat;
+  }
+}
+
+class TelegramBot_FactoryEventGroupMe extends TelegramBot_FactoryEvent{
+  public int $Action = 0;
+  public function __construct(){
+    $this->Type = TelegramBot_Basics::Event_GroupMe;
+  }
+}
+
+class TelegramBot_FactoryEventGroupUpdate extends TelegramBot_FactoryEvent{
+  public int $Action = 0;
+  public function __construct(){
+    $this->Type = TelegramBot_Basics::Event_GroupUpdate;
+  }
 }
